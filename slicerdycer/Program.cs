147be28312b924301004234ddfeecdecb2752ad2
forReason <bechti44@googlemail.com>
2017-06-13 14:39:35 +0200

    fixed a calculation bug for trailing bet

--- slicerdycer/Program.cs
+++ slicerdycer/Program.cs
@@ -107,8 +107,8 @@ namespace slicerdycer
                 //otherwhise keep distance so that at least 12 times in a row can be a loss (happens 1 out of 4k rolls stochastically)
                 else
                 {
-                    Networkhandler.Bet((int)Math.Pow(0.04 * GlobalVar.balance[user], 0.5)+1, user);
-                    GlobalVar.betting[user] = (int)Math.Pow(0.04 * GlobalVar.balance[user], 0.5) + 1;
+                    Networkhandler.Bet((int)(0.04 * Math.Pow(GlobalVar.balance[user], 0.5)+1), user);
+                    GlobalVar.betting[user] = (int)(0.04 * Math.Pow(GlobalVar.balance[user], 0.5) + 1);
                     GlobalVar.firstnegative[user] = true;
                 }
                 //if last bid was the first negative, dont rise to het the possible failovers higher
